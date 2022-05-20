7d3b501ae2ba6daeef98a1834933ceedf130eb24
lijinyu <823584301@qq.com>
2022-05-20 14:36:23 +0800

    自动导入

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import Components from "unplugin-vue-components/vite";
+import AutoImport from "unplugin-auto-import/vite"; // 自动导入vue3函数包
+import { AntDesignVueResolver } from "unplugin-vue-components/resolvers"; // 引入ui
+
+import * as path from "path";
+// 自动导入config
+const componentsConfig = {
+  dts: true, // 使用ts时
+  resolvers: [AntDesignVueResolver()],
+};
+
+const autoImportConfig = {
+  imports: ["vue", "vue-router", {}],
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue(), Components(componentsConfig), AutoImport(autoImportConfig)],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
